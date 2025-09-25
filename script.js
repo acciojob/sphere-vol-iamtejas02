940f2af82da386737f1d83039accc0b1c303b585
Iamtejas <tejasbhajbhuje.dev@gmail.com>
2025-09-25 11:01:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
 function volume_sphere() {
-    //Write your code here
-  
-} 
+	
+    let radius = document.getElementById("radius").value;
+
+    radius = parseFloat(radius);
+
+    let volume = (radius >= 0 && !isNaN(radius))
+        ? ((4 / 3) * Math.PI * Math.pow(radius, 3)).toFixed(4)
+        : NaN;
+	
+    document.getElementById("volume").value = volume;
+}
+
+
+document.getElementById("MyForm").addEventListener("submit", function(event) {
+    event.preventDefault(); // prevent page reload
+    volume_sphere();
+});
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
